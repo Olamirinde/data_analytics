cd05458ff4d8cbb7ae62658ea542dd72dcb4a189
Your Name <rindepixels@gmail.com>
2023-08-03 11:43:03 +0100

    fix import error

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
 import Newsletter from "./Components/Newsletter";
-import Cards from "./Components/cards";
+import Cards from "./Components/Cards";
 import Footer from "./Components/Footer";
 
 function App() {
